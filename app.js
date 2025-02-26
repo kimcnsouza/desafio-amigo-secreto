ca5a17eea992262b2017eeb08047ab7a90305408
Kimberly <kimberlydesouza0810@gmail.com>
2025-02-25 22:17:44 -0300

    adicionar nome

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-//teste
+let listaNomes = [];
+
+//função para inserir nomes no input
+function adicionarNome() {
+    let nomes = document.querySelector('#amigo').value;
+    console.log(nomes);
+    
+    //limpar os campos
+    let limparInput = document.querySelector('#amigo');
+    limparInput.value = '';
+}
+
+//verificar se o campo foi preenchido
+//habilitar o botão de adicionar nome
+//armazenar e exibir o nome na tela 
+//sortear 
+
