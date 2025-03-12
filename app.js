3a58a9bcaf7e0cd08f6d63ab3ed9393a23ddbfb7
Kimberly <kimberlydesouza0810@gmail.com>
2025-03-11 22:27:20 -0300

    projeto final

--- app.js
+++ app.js
@@ -1,17 +1,56 @@
+//para pegar a lista
+let lista = document.querySelector('#listaAmigos');
+//para colocar o resultado
+let resultado = document.querySelector('#resultado');
+//para armazenar os nomes e número de nomes na lista
 let listaNomes = [];
+let numeroDeNomes = 0;
 
 //função para inserir nomes no input
 function adicionarNome() {
     let nomes = document.querySelector('#amigo').value;
-    console.log(nomes);
+
+
+    //verifica se o nome inserido é valido
+    if (nomes === '') {
+        alert('Digite um nome')
+    } else {
+        //coloca os nomes na lista
+         listaNomes.push(nomes)
+        
+         //para adicionar os nomes    
+         lista.innerHTML = listaNomes.map(nome => `<li>${nome}</li>`).join('');
+     
+         //limpar os campos
+         let limparInput = document.querySelector('#amigo');
+         limparInput.value = '';
+        
+         //conta o número de nomes na lista
+         numeroDeNomes++;
+     
+         //mostra numero de nomes na lista
+         console.log(numeroDeNomes)
+     
+         //para mostrar a lista no console
+         console.log(listaNomes);  
+    }
     
-    //limpar os campos
-    let limparInput = document.querySelector('#amigo');
-    limparInput.value = '';
 }
 
-//verificar se o campo foi preenchido
-//habilitar o botão de adicionar nome
-//armazenar e exibir o nome na tela 
 //sortear 
+function sortearAmigo() {
+    //valida se há nomes na lista
+    if (numeroDeNomes <= 1) {
+        alert('Insira um nome na lista!')
+    } else {
+        //sorteia um indice da lista
+        let numeroSorteado = parseInt(Math.random() * numeroDeNomes);
 
+        //exibe nome na tela
+        resultado.innerHTML = listaNomes[numeroSorteado];
+
+        //exibe o indice sorteado no console
+        console.log(`O indice sorteado é... ${numeroSorteado}`)
+    }
+    
+}
